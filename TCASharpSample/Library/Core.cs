c4a52d156c7f845b0a576695a46dbafa46fc4a95
Sergio Daniel <me@zheref.io>
2025-05-03 16:50:44 -0500

    Add Mvvm package

---
+++ TCASharpSample/Library/Core.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCASharpSample.Library;
+
+internal class Store<Value>
+{
+    Value value { get; set; }
+
+    internal Store(Value initialValue)
+    {
+        this.value = value;
+    }
+}
